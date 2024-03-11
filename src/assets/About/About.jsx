b0b3f0fcaedd238eeb2b3d76c044bd32713d7f3d
Josh Shire <joshshirechildren@gmail.com>
2024-03-11 17:07:41 -0600

    Backup

--- src/assets/About/About.jsx
+++ src/assets/About/About.jsx
@@ -5,8 +5,8 @@ import banner from './Banner.jpg'
 import backgroundImage from './PlaceHolderDog.jpg';
 import projectImage from './Project.jpg';
 import Footer from '../Footer/Footer';
-function About(){
 
+function About(){
     let [color, setColor] = useState(() => localStorage.getItem('color') || '#333')
     useEffect(() => {
         const colorItem = localStorage.getItem('color');
@@ -19,25 +19,29 @@ function About(){
     return(
         <div id={styles.container}>
             <Sidebar/>
-            <div className={styles.Who}>
-                <h2>Hello, I'm Josh.</h2>
-                <h4>I'm learning to become a</h4>
-                <h2 className={styles.subject}>Full Stack</h2>
-                <h2 className={styles.subject}>Developer</h2>
-                <button className={styles.button}value='Linkden'>Download CV</button>
-                <img className={styles.whoImage} src={backgroundImage}></img>
+            <main>
+                <div className={styles.Who}>
+                    <h2>Hello, I'm Josh.</h2>
+                    <h4>I'm learning to become a</h4>
+                    <h2 className={styles.subject}>Full Stack</h2>
+                    <h2 className={styles.subject}>Developer</h2>
+                    <button className={styles.button}value='Linkden'>Download CV</button>
+                    <img className={styles.whoImage} src={backgroundImage}></img>
                 </div>
                 <div className={styles.projects}>
                     <h1 className={styles.projectTitle}>My projects</h1>
                 </div>
-                    <div className={styles.project1}>
-                        <a href='/Home'><h3 className={styles.Project1Title}>Project 1 - Website</h3></a>
-                        <pre className={styles.Project1Desc}>Lorem ipsum, dolor sit amet<br></br>
-                        consectetur adipisicing elit. Possimus.</pre>
-                        <img className={styles.Project1Image} src={projectImage}></img>
-                    </div>
-                    <Footer/>
-            </div>
+                <div className={styles.project1}>
+                    <a href='/Home'><h3 className={styles.Project1Title}>Project 1 - Website</h3></a>
+                    <pre className={styles.Project1Desc}>Lorem ipsum, dolor sit amet<br></br>
+                    consectetur adipisicing elit. Possimus.</pre>
+                    <img className={styles.Project1Image} src={projectImage}></img>
+                </div>
+            </main>
+            <footer className={styles.footer}>
+                <p>© 2024 Josh. All rights reserved.</p>
+            </footer>
+        </div>
     );
 }
 
--- src/assets/Footer/Footer.jsx
+++ src/assets/Footer/Footer.jsx
@@ -1,11 +1,9 @@
 import styles from './Footer.module.css'
 function Footer(){
     return(
-        <div>
             <footer className={styles.footer}>
                 <p>© 2024 Josh. All rights reserved.</p>
             </footer>
-        </div>
     );
 }
 export default Footer
